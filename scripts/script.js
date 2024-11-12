084f77d6a4ac6f58a81a66b87fa09a1d8315e16f
JBB0807 <104856796+JBB0807@users.noreply.github.com>
2024-11-12 12:30:45 -0800

    Added player name feature, difficulty updates and game status updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
-'use strict';
+"use strict";
 
 const gameBoard = {
+  playerName: null,
+  scrore: null,
+  difficultyOptions: ["Easy", "Normal", "Hard"],
+  diffifucltyIndex: 1,
   props: [],
   isRunning: false,
   keyframe: null,
@@ -9,49 +13,86 @@ const gameBoard = {
   activeScreen: null,
 
   init() {
-    this.switchScreen("#screen-main")
+    this.switchScreen("#screen-main");
+    this.adjustDifficulty(0)
   },
 
-  switchScreen(screenID){
-    if(screenID !== "#screen-pause") {
-        $(".div-screen").hide();
-    } 
+  startGame() {
+    this.playerName = $("#input-player-name").val();
+    this.isRunning = true;
+    console.log(`Game Started: Welcome ${this.playerName}!`);
+  },
+
+  pauseGame() {
+    this.isRunning = false;
+    console.log(`Game Paused`);
+  },
+
+  resumeGame(){
+    this.isRunning = true;
+    console.log(`Game Resumed`);
+  },
+
+  adjustDifficulty(intPlusMinus) {
+    this.diffifucltyIndex += intPlusMinus;
+    if (this.diffifucltyIndex < 0) {
+      this.diffifucltyIndex = this.difficultyOptions.length - 1;
+    } else if (this.diffifucltyIndex >= this.difficultyOptions.length) {
+      this.diffifucltyIndex = 0;
+    }
+
+    $("#difficulty-level-display").text(
+      this.difficultyOptions[this.diffifucltyIndex]
+    );
+  },
+
+  switchScreen(screenID) {
+    if (screenID !== "#screen-pause") {
+      $(".div-screen").hide();
+    }
     $(screenID).show();
     this.activeScreen = screenID;
-  }
-
+  },
 };
 
-$(document).ready(function() {
-    gameBoard.init();
-    setupListeners();
+$(document).ready(function () {
+  gameBoard.init();
+  setupListeners();
 });
 
 function setupListeners() {
-    //
-    //on click listener for all menu items that will trigger a screen to change
-    $(".div-screen-select").on("click", function () {
-        if($(this).hasClass("switch-screen-play")){
-            console.log("Switch to play screen");
-            gameBoard.switchScreen("#screen-play");
-            
-        } else if($(this).hasClass("switch-screen-main")){
-            console.log("Switch to main screen");
-            gameBoard.switchScreen("#screen-main");
+  //
+  //on click listener for all menu items that will trigger a screen to change
+  $(".div-screen-select").on("click", function () {
+    var elementId = $(this).attr("id");
+    if ($(this).hasClass("switch-screen-play")) {
+      console.log("Switch to play screen");
+      gameBoard.switchScreen("#screen-play");
+        if(elementId === "menu-start-game") {
+            gameBoard.startGame();
+        } else if (elementId === "menu-resume") {
+            gameBoard.resumeGame();
+        }
+    } else if ($(this).hasClass("switch-screen-main")) {
+      console.log("Switch to main screen");
+      gameBoard.switchScreen("#screen-main");
+    } else if ($(this).hasClass("switch-screen-pause")) {
+      console.log("Switch to pause screen");
+      gameBoard.pauseGame();
+      gameBoard.switchScreen("#screen-pause");
+    } else if ($(this).hasClass("switch-screen-credits")) {
+      console.log("Switch to credits screen");
+      gameBoard.switchScreen("#screen-credits");
+    }
+  });
 
-        } else if($(this).hasClass("switch-screen-pause")){
-            console.log("Switch to pause screen");
-            gameBoard.switchScreen("#screen-pause");
+  $("#difficulty-adjust-right").on("click", function () {
+    gameBoard.adjustDifficulty(1);
+  });
 
-        } else if($(this).hasClass("switch-screen-credits")){
-            console.log("Switch to credits screen");
-            gameBoard.switchScreen("#screen-credits");
-
-        } 
-    });
-
-    
-
+  $("#difficulty-adjust-left").on("click", function () {
+    gameBoard.adjustDifficulty(-1);
+  });
 }
 
 function getRandomNumber(min, max) {
